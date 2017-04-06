4258cdeb7ff74b80c25200349e02168c57e7298e
Xiaotong <Xiaotong-robillard@hotmail.com>
2017-04-06 14:04:17 -0400

    déclaration fini

--- UnitDeclarations.pas
+++ UnitDeclarations.pas
@@ -6,7 +6,6 @@ const
 NB_MAX_ELEVES = 25; (* Nombre maximum d'élèves *)
 NB_EVAL = 4;
 
-implementation
 
 type
 TTabNomsEleves = array [0..NB_MAX_ELEVES-1] of string;
@@ -40,4 +39,5 @@ TAB_NOMS_INFO : TTabNomsInfo (* Tableau des noms des informations sur *)
 = ('Nom de l''élève', 'Examen #1', 'Examen #2', (* les élèves *)
 'Travail #1', 'Travail #2', 'Finale');
 
+implementation
 end.
--- UnitfchAjout.pas
+++ UnitfchAjout.pas
@@ -21,8 +21,20 @@ type
     cmdConfirmer: TButton;
     cmdAnnuler: TButton;
     fchAjout: TStatusBar;
+
   private
     { Déclarations privées }
+
+  TabNomsAjt : TTabNomsEleves;//Un tableau du nom de chacun des élèves du groupe
+
+  TabEvalAjt : TTabLesEval;{2D un tableau des notes des évaluations et notes
+  finales de chaque élève du groupe (les notes finales seront calculée plus tard}
+
+  NbElevesAjt : integer; //le nombre d'élèves dans le groupe
+
+  IndAjt : boolean; {booléen utilisé pour indiquer s'il y a eu ou non des ajouts
+   d'élèves.}
+
   public
     { Déclarations publiques }
   end;
